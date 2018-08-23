8a88c2a510db50a8efb74c249872eb67d26f9725
chenkaijie <chenkaijie4ever@gmail.com>
2018-08-24 00:07:15 +0800

    引入Redux来管理我们的主题色

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
 import PropTypes from 'prop-types'
 import Header from './Header'
 import Content from './Content'
@@ -14,5 +15,19 @@ class Index extends Component {
         )
     }
 }
+
+const themeReducer = function (state, action) {
+    if (!state) return {
+        themeColor: 'red'
+    }
+    switch (action.type) {
+        case 'CHANGE_COLOR':
+            return { ...state, themeColor: action.themeColor }
+        default:
+            return state
+    }
+}
+
+const store = createStore(themeReducer)
 
 ReactDOM.render(<Index />, document.getElementById('root'));
